d8cf6a884650e20a032a7b6f209071356fc3903e
MateusBirtan <contatobirtan@gmail.com>
2024-05-25 18:21:04 -0400

    Add CryptoCurrencyData Inteface

---
+++ interfaces/types.ts
@@ -0,0 +1,28 @@
+export interface CryptoCurrencyData {
+  stream: string;
+  data: {
+    e: string;
+    E: number;
+    s: string;
+    p: string;
+    P: string;
+    w: string;
+    x: string;
+    c: string;
+    Q: string;
+    b: string;
+    B: string;
+    a: string;
+    A: string;
+    o: string;
+    h: string;
+    l: string;
+    v: string;
+    q: string;
+    O: number;
+    C: number;
+    F: number;
+    L: number;
+    n: number;
+  };
+}
