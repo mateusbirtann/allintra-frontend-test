d0a6d0a7702d88c8f667ec6889586687506614d7
MateusBirtan <contatobirtan@gmail.com>
2024-05-27 04:30:09 -0400

    refactor: Normalize percentage change calculation

--- store/reducer.ts
+++ store/reducer.ts
@@ -1,14 +1,12 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface CryptoState {
-  data: Array<{ name: string; value: string }>;
   firstValues: { [key: string]: string };
   currentValues: { [key: string]: string };
   percentageChange: { [key: string]: number };
 }
 
 const initialState: CryptoState = {
-  data: [],
   firstValues: {},
   currentValues: {},
   percentageChange: {},
@@ -19,8 +17,6 @@ export const cryptoCurrencySlice = createSlice({
   initialState,
   reducers: {
     addCryptoData: (state, action: PayloadAction<{ name: string; value: string }>) => {
-      // state.data.push(action.payload);
-
       // Check if this is the first value for this cryptocurrency
       if (!state.firstValues[action.payload.name]) {
         state.firstValues[action.payload.name] = action.payload.value;
@@ -33,9 +29,11 @@ export const cryptoCurrencySlice = createSlice({
       for (const key in state.currentValues) {
         const firstValue = parseFloat(state.firstValues[key]);
         const currentValue = parseFloat(state.currentValues[key]);
-        const percentageChange = ((currentValue - firstValue) / firstValue) * 100;
+        const priceChangePercentage = ((currentValue - firstValue) / firstValue) * 100;
+        const absolutPercentageChange = Math.abs(priceChangePercentage);
+        const normalizedPercentageChange = absolutPercentageChange / 100;
 
-        state.percentageChange[key] = percentageChange;
+        state.percentageChange[key] = normalizedPercentageChange;
       }
     },
   },
