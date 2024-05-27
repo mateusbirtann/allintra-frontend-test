a8f8899f060a66dc47998640945349f17c3f9c8e
MateusBirtan <contatobirtan@gmail.com>
2024-05-26 23:04:09 -0400

    refactor: Update Card and Table components for improved UI consistency

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -6,7 +6,7 @@ const Card = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElemen
   ({ className, ...props }, ref) => (
     <div
       ref={ref}
-      className={cn('rounded-lg border bg-card text-card-foreground shadow-sm', className)}
+      className={cn('rounded-lg bg-card text-card-foreground', className)}
       {...props}
     />
   ),
@@ -40,9 +40,7 @@ const CardDescription = React.forwardRef<
 CardDescription.displayName = 'CardDescription';
 
 const CardContent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLDivElement>>(
-  ({ className, ...props }, ref) => (
-    <div ref={ref} className={cn('p-6 pt-0', className)} {...props} />
-  ),
+  ({ className, ...props }, ref) => <div ref={ref} className={cn(className)} {...props} />,
 );
 CardContent.displayName = 'CardContent';
 
